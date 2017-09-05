fe92afeb6041216e6bbc1fbe8b94474dc0c530d4
Dave Perlman <daveperlman89@me.com>
2017-09-05 16:48:41 -0400

    init commit

---
+++ index.php
@@ -0,0 +1,199 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title></title>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+		<style>
+			.row {
+				padding: 5rem 0;
+			}
+		</style>
+	</head>
+	<body class="container-fluid" style="padding-right: 0; padding-left: 0;">
+		<header class="navbar navbar-default navbar-toggleable">
+			<nav class="container-fluid">
+				<a href="" class="navbar-brand">Refined Tech</a>
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#dropdown">
+        			<span class="icon-bar"></span>
+        			<span class="icon-bar"></span>
+        			<span class="icon-bar"></span> 
+      			</button>
+				<div class="navbar-collapse collapse" id="dropdown">
+					<ul class="nav navbar-nav navbar-right">
+						<li class="nav-item"><a href="">About</a></li>
+						<li class="nav-item"><a href="">Things</a></li>
+						<li class="nav-item"><a href="">Work</a></li>
+					</ul>
+				</div>
+			</nav>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+		</header>
+		<div class="jumbotron" style="background: none;">
+			<div class="container">
+				<h1>Refined Tech</h1>
+				<hr>
+				<div class="col-sm-6">
+					<h2>Creativity.</h2>
+					<p>In Web Solutions</p>
+					<div class="btn btn-warning">Get Answers.</div>
+				</div>
+			</div>
+		</div>
+		<div class="container-fluid">
+			<div class="row" style="background: linear-gradient(to bottom right, #FFF, #fff, #dee);">
+			<div class="container">
+				<div class="col-sm-6">
+					<h2>Dynamic Infrastructure</h2>
+					<p class="col-sm-12">Refined Technologies is web development company specializing in data solutions. We offer a variety of services and solutions to your <strong>web presence</strong> needs. </p>
+				</div>
+				<div class="col-sm-6">
+					<img class="img-responsive" src="infra.png" alt="">
+				</div>
+			</div>
+			<div class="row">
+				<!-- <p class="col-sm-4 col-sm-offset-4">Refined Technologies is web development company specializing in data solutions. We offer a variety of services and solutions to your <strong>web presence</strong> needs. </p> -->
+			</div>
+		</div>
+		<div class="row" style="background: linear-gradient(to top right, #fff, #fff, #dee);">
+			<div class="container">
+				<h2 class="">The Refined Process</h2>
+				<div class="col-sm-6 right">
+					<img class="img-responsive" src="tech.png" alt="">
+				</div>
+				<div class="col-sm-6">
+					<!-- <ul class="list-group">
+						<li class="list-group-item">Responsive, modern web sites</li>
+						<li class="list-group-item">Solutions to complex data problems</li>
+						<li class="list-group-item">Boosting efficiency with web applications</li>
+					</ul> -->
+					<div class="panel-group" id="accordian">
+						<div class="panel panel-default">
+							<div class="panel-heading">
+								<h4 class="panel-title">
+									<a href="#collapseOne" data-toggle='collapse' data-parent='#accordian'>
+										Overview
+									</a>
+								</h4>
+							</div>
+							<div class="panel-collapse collapse in" id="collapseOne">
+								<div class="panel-body">
+									Our approach to site building utilizes current best practices and the latest in web technologies. We pride ourselves on delivering stable and secure finished products to our clients. 
+								</div>
+							</div>
+						</div>
+						<div class="panel panel-default">
+							<div class="panel-heading">
+								<h4 class="panel-title">
+									<a href="#collapseTwo" data-toggle='collapse' data-parent='#accordian'>
+										Foundation
+									</a>
+								</h4>
+							</div>
+							<div class="panel-collapse collapse" id="collapseTwo">
+								<div class="panel-body">
+									A typical shortfall in modern development is a lax approach to backend development. Your site or app is only as strong as it's foundation. Our clients look for a website that is instrumental in stream-lining processes, collecting marketing data, and functioning as a tool as opposed to just a landing page.
+								</div>
+							</div>
+						</div>
+						<div class="panel panel-default">
+							<div class="panel-heading">
+								<h4 class="panel-title">
+									<a href="#collapseThree" data-toggle='collapse' data-parent='#accordian'>
+										Optimization
+									</a>
+								</h4>
+							</div>
+							<div class="panel-collapse collapse" id="collapseThree">
+								<div class="panel-body">
+									Mobile devices are used to search the web now more than any other device. Modern best practice dictates that web sites should be optimized for these devices for ease of use. With input from our clients, we design our sites to be easily and quickly navigated on all devices.
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="container">
+				<h2>My Work</h2>
+				<div class="col-sm-4">
+					<div class="panel panel-default">
+						<div class="panel-heading">
+							<h4 class="panel-title text-center">
+								Data Solutions
+							</h4>
+						</div>
+						<img src="data.png" alt="" class="img-responsive">
+						<div class="panel-body">
+							<p>In our eyes, a website should be a marketing tool as well as a processing tool. For many clients, a web based application can improve efficiency in many facets of business and help to create an easier and more organized process. </p>
+						</div>
+					</div>	
+				</div>
+				<div class="col-sm-4">
+					<div class="panel panel-default">
+						<div class="panel-heading">
+							<h4 class="panel-title text-center">
+								Dynamic Websites
+							</h4>
+						</div>
+						<img src="dynamic.png" alt="" class="img-responsive">
+						<div class="panel-body">
+							<p>Being able to change content on a website is important to many clients. Regularly posting new content not only keeps your website appealing to viewers, it also helps to optimize a site for placement in search results. We offer a number of solutions for clients looking to have more control over their web presence.</p>
+						</div>
+					</div>	
+				</div>
+				<div class="col-sm-4">
+					<div class="panel panel-default">
+						<div class="panel-heading">
+							<h4 class="panel-title text-center">
+								SEO
+							</h4>
+						</div>
+						<img src="seo.png" alt="" class="img-responsive">
+						<div class="panel-body">
+							<p>Search Engine Optimization is an integral part of modern web development. It assists search engines in indexing a web site correctly. As an example, you wouldn't want a site that sells fishing poles to be indexed when someone searches for bowling shoes - but you would want that site indexed when someone searches for "fishing poles online". By using a number of strategies, we optimize our clients sites for the major search engines.</p>
+						</div>
+					</div>	
+				</div>
+			</div>
+		</div>
+	</div>
+		<div class="footer" style="background: linear-gradient(to bottom, #fff, #fff, #dee)">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-4">
+							<div class="well">
+								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo itaque maiores dolorum recusandae dignissimos excepturi, aliquid porro accusantium sed, inventore ex modi soluta, repellendus rerum placeat doloremque culpa odio iste!</p>
+							</div>
+						</div>
+						<div class="col-md-4">
+							<div class="text-center center-block">
+								<a href="#">
+									<i class="glyphicon glyphicon-envelope"></i>
+								</a>
+								<a href="#">
+									<i class="glyphicon glyphicon-envelope"></i>
+								</a>
+								<a href="#">
+									<i class="glyphicon glyphicon-envelope"></i>
+								</a>
+							</div>
+						</div>
+						<div class="col-md-4">
+							<div class="well">
+								<form action="#">
+									<div class="input-group">
+										<input type="text" class="btn btn-lg" name="email" id="email" type="email" required placeholder="Your Email">
+										<button class="btn btn-info btn-lg" type="submit">Submit</button>
+									</div>
+								</form>
+							</div>
+						</div>
+					</div>
+				</div>
+		</div>
+		<script src='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js'></script>
+	</body>
+</html>
